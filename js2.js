0b3829feab3dd4b238fec94492f07ef6432c87c4
illiabutsenko <ilya1990.0826@gmail.com>
2023-12-06 21:03:54 +0100

    JavaScript. Variables, Types, Operators

---
+++ js2.js
@@ -0,0 +1,34 @@
+let number1 = 10;
+let number2 = 5;
+let sum = number1 + number2;
+
+// console.log(sum);
+
+let age;
+age = 25;
+// age = 3.14;
+// age = 'Hi';
+// age = "JavaScript";
+// age = true;
+// age = undefined;
+// age = null;
+// let sum = 10 + 5;
+// let result = 10 - 5;
+// let result = 10 * 5;
+// let result = 10 / 5;
+// let result = 5 == 5;
+// let result = 5 == "5";
+// let result = 12 == 5;
+// let result = 5 === 5;
+// let result = 5 === "5";
+// let result = 5 != 4;
+// let result = 5 != "4";
+// let result = 5 != "4";
+// let result = 10 > 4;
+// let result = 10 > "4";
+// let result = 10 < "4";
+// let result = true && false;
+let result = true || false;
+// let result = !true;
+
+console.log(sum, 'and', result);
