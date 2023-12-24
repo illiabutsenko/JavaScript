b3d58f442abcccb37b4b9202b8589f885b7a7d01
illiabutsenko <ilya1990.0826@gmail.com>
2023-12-24 19:53:20 +0100

    JavaScript. lexical environment

--- js29Eventloop.js
+++ js29Eventloop.js
@@ -8,6 +8,21 @@ setTimeout(() => {
 }, 2000);
 console.log("Time4");
 
-//Після виконання Time1 JavaScript зареєстрував 
-//callback на виконання ф-ї(Time2 і Time3), та продовжує 
+//Після виконання Time1 JavaScript зареєстрував
+//callback на виконання ф-ї(Time2 і Time3), та продовжує
 //виконувати інші завдання(Time4);
+
+
+
+
+
+//lexical environment
+let a = 3;
+function f1() {
+  let b = 2;
+  return function f2() {
+    console.log(a, b);
+  };
+}
+let c = f1();
+console.log(c());
